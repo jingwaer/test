74705e7cf4f6f2ae869189e07674fcfaa383fd01
jingwaer <568748867@qq.com>
2020-11-16 17:31:57 +0800

    提交

---
+++ src/exercise/test/HelloWorld.java
@@ -0,0 +1,14 @@
+package exercise.test;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello!");
+		String a = "123";
+		String b = "1"+"23";
+		String c = "1";
+		String d = c+"23";
+		System.out.println(a==b);
+		System.out.println(a==d);
+	}
+
+}
